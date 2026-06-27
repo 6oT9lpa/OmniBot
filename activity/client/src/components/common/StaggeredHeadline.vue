65b0f8014e2682c2c063fa230213ed430cd14025
6ot9lpa <sp1vaks@yandex.ru>
2026-06-27 14:39:44 +0300

    Add typewriter headline animation

--- activity/client/src/components/common/StaggeredHeadline.vue
+++ activity/client/src/components/common/StaggeredHeadline.vue
@@ -1,30 +1,64 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, onBeforeUnmount, onMounted, ref, watch } from "vue";
 
 const props = withDefaults(
   defineProps<{
     text: string;
     as?: string;
+    speed?: number;
   }>(),
   {
     as: "h1",
+    speed: 42,
   },
 );
 
 const characters = computed(() => Array.from(props.text));
+const visibleCount = ref(0);
+let timerId: number | undefined;
+
+function clearTypingTimer() {
+  if (timerId !== undefined) {
+    window.clearInterval(timerId);
+    timerId = undefined;
+  }
+}
+
+function startTyping() {
+  clearTypingTimer();
+  visibleCount.value = 0;
+
+  if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    visibleCount.value = characters.value.length;
+    return;
+  }
+
+  timerId = window.setInterval(() => {
+    visibleCount.value += 1;
+    if (visibleCount.value >= characters.value.length) {
+      visibleCount.value = characters.value.length;
+      clearTypingTimer();
+    }
+  }, props.speed);
+}
+
+watch(() => props.text, startTyping);
+
+onMounted(startTyping);
+onBeforeUnmount(clearTypingTimer);
 </script>
 
 <template>
-  <component :is="props.as" class="staggered-headline" :aria-label="props.text">
+  <component :is="props.as" class="staggered-headline typewriter-headline" :aria-label="props.text">
     <span
-      v-for="(character, index) in characters"
+      v-for="(character, index) in characters.slice(0, visibleCount)"
       :key="`${character}-${index}`"
-      class="staggered-letter"
+      class="typewriter-letter"
       :class="{ 'is-space': character === ' ' }"
-      :style="{ '--letter-index': index }"
       aria-hidden="true"
     >
       {{ character === " " ? "\u00A0" : character }}
     </span>
+    <span class="typewriter-cursor" aria-hidden="true"></span>
   </component>
 </template>
